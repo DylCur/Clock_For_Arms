045da079abb4363d11cfdc036fa8670a985897bc
DylCur <dylancurry001@gmail.com>
2023-04-26 18:22:19 +0100

    Update playerController.cs

--- Assets/Scripts/Player/playerController.cs
+++ Assets/Scripts/Player/playerController.cs
@@ -12,8 +12,11 @@ public class playerController : MonoBehaviour
 
     [HideInInspector] public float horizontalInput;
     [HideInInspector] public float verticalInput;
+    [HideInInspector] public bool canRotate = false; 
+    [HideInInspector] public bool canMove = true;
 
     Rigidbody2D rb;
+    
 
 
 
@@ -23,22 +26,41 @@ public class playerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        if(!canMove){
+            Debug.Log("Can move is false");
+        }
+
+        if(!canRotate){
+            Debug.Log("Can rotate is false");
+        }
     }
 
     
     void FixedUpdate()
     {
-        
-        movement(playerMoveSpeed, horizontalInput = Input.GetAxisRaw("Horizontal"), verticalInput = Input.GetAxisRaw("Vertical")); // Inputs the speed and the inputs 😲
+        if(canMove){
+            Movement(playerMoveSpeed, horizontalInput = Input.GetAxisRaw("Horizontal"), verticalInput = Input.GetAxisRaw("Vertical")); // Inputs the speed and the inputs 😲
+        }
+
+        if(canRotate){
+            RotatePlayer(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        }
     }
 
-    public void movement(float playerMoveSpeed, float horizontalInput, float verticalInput){
+    public void Movement(float playerMoveSpeed, float horizontalInput, float verticalInput){
 
         
 
-        Vector2 movementVector = new Vector2(horizontalInput, verticalInput);
+        Vector2 movementVector = new Vector2(horizontalInput * playerMoveSpeed, verticalInput * playerMoveSpeed);
 
         rb.velocity = movementVector;
 
+    }
+
+    public void RotatePlayer(Vector3 mousePosition){
+        
+        float angle = Mathf.Atan2(mousePosition.y - transform.position.y, mousePosition.x - transform.position.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
     }
 }
