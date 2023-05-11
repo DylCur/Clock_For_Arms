a1a0c45b4727255ec2bfa84912e27933fc3f3853
DylCur <dylancurry001@gmail.com>
2023-05-11 22:27:59 +0100

    More map, sprites and removed the error on enemy hit

--- Assets/[]Scripts/Combat/hitDetector.cs
+++ Assets/[]Scripts/Combat/hitDetector.cs
@@ -34,33 +34,33 @@ public class hitDetector : MonoBehaviour
     }
 
 
-    public void OnTriggerEnter2D(Collider2D other) {
-        foreach(possibleEnemyTags value in Enum.GetValues(typeof(possibleEnemyTags))){
-            if(other.tag == value.ToString() && !hasHit){
+    // public void OnTriggerEnter2D(Collider2D other) {
+    //     foreach(possibleEnemyTags value in Enum.GetValues(typeof(possibleEnemyTags))){
+    //         if(other.tag == value.ToString() && !hasHit){
 
                 
      
-                enemyProperties enemProp = other.GetComponent<enemyProperties>();
-                if(enemProp != null){
+    //             enemyProperties enemProp = other.GetComponent<enemyProperties>();
+    //             if(enemProp != null){
                     
-                    //enemProp.TakeDamage(attackControl.swordAttackDamage);
-                    Debug.Log($"Not Null {other.gameObject}");
-                }
+    //                 //enemProp.TakeDamage(attackControl.swordAttackDamage);
+    //                 Debug.Log($"Not Null {other.gameObject}");
+    //             }
 
-                else{
-                    Debug.Log($"Null {other.gameObject}");
+    //             else{
+    //                 Debug.Log($"Null {other.gameObject}");
 
-                }
+    //             }
 
 
-                hasHit = true;
-                StartCoroutine(WaitAttack());
-            }
-        }
-    }
+    //             hasHit = true;
+    //             StartCoroutine(WaitAttack());
+    //         }
+    //     }
+    // }
 
-    public IEnumerator WaitAttack(){
-        yield return new WaitForSeconds(attackControl.timeBetweenAttack * 1.1f);
-        hasHit = false;
-    }
+    // public IEnumerator WaitAttack(){
+    //     yield return new WaitForSeconds(attackControl.timeBetweenAttack * 1.1f);
+    //     hasHit = false;
+    // }
 }
